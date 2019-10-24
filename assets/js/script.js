4a28afd198effa55e9436c41bdeab1fcb64c482b
Francesco <franc.ns@gmail.com>
2019-10-24 15:03:13 +0200

    added js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,25 @@
 // ma per i multipli di 3 stampi “Fizz” al posto del numero
 // e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// creazione variabili
+var fizz, buzz, fizzbuzz, output, i;
+
+// assegnazione variabili
+fizz = 'Fizz';
+buzz = 'Buzz';
+fizzbuzz = 'FizzBuzz';
+
+// ciclo per stampa
+for (i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    output = fizzbuzz;
+  } else if (i % 3 == 0) {
+    output = fizz;
+  } else if (i % 5 == 0) {
+    output = buzz;
+  } else {
+    output = i;
+  }
+  console.log(output);
+}
